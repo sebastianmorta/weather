0ec0156005db4faf626575804ec4a46dc1da1cf7
sebastianmorta <mr.mchaker@gmail.com>
2020-07-28 02:05:37 +0200

    add new features

--- script.js
+++ script.js
@@ -39,7 +39,11 @@ function getWeather() {
     let temperature = document.getElementById("temperature");
     let description = document.getElementById("description");
     let location = document.getElementById("location");
-  
+    let date = document.getElementById("date");
+    let humidity = document.getElementById("humidity");
+    let pressure = document.getElementById("pressure");
+    let clouds = document.getElementById("clouds");
+
     let api = "https://api.openweathermap.org/data/2.5/weather";
     let apiKey = "e5799fe6d69575dde12268e5e571c8fa";
   
@@ -59,14 +63,26 @@ function getWeather() {
         longitude +
         "&appid=" +
         apiKey +
-        "&units=imperial";
+        "&units=metric";
+
+
+        
+        
   
       fetch(url)
         .then(response => response.json())
         .then(data => {
           console.log(data);
+          let c=new Date(data.dt*1000);
+          date.innerHTML=c;
           let temp = data.main.temp;
-          temperature.innerHTML = temp + "° F";
+          temperature.innerHTML = temp + "°C";
+          let hum = data.main.humidity;
+          humidity.innerHTML= hum + "%";
+          let pres = data.main.pressure;
+          pressure.innerHTML= pres+ "hPa";
+          let clou = data.clouds.all;
+          clouds.innerHTML= clou+ "hPa";
           location.innerHTML =
             data.name + " (" + latitude + "°, " + longitude + "°)";
           description.innerHTML = data.weather[0].main;
